5bc61148ca0d98a30ccafa03def766f64f3e0350
Ramwis <alexander.krobot@gmail.com>
2025-06-16 10:40:05 +0200

    jupiii
    opäť aspoň sa mi načíta stránka. A ktokoľvek bude čítať tieto commity. (Sú to výkriky zúfalstva.)

--- scholar/Includes/banner.php
+++ scholar/Includes/banner.php
@@ -1,25 +1,38 @@
 <?php
-// Define banner slides (buttons removed)
-
-// Pripojenie k DB
-$mysqli = new mysqli('localhost', 'username', 'password', 'database');
-if ($mysqli->connect_error) {
-    die('Chyba pripojenia: ' . $mysqli->connect_error);
-}
-
-// Načítanie bannerov z DB
-$result = $mysqli->query("SELECT * FROM banners ORDER BY id ASC");
-
-$bannerSlides = [];
-while ($row = $result->fetch_assoc()) {
-    $bannerSlides[] = [
-        'item_class' => $row['item_class'],
-        'category' => $row['category'],
-        'title' => $row['title'],
-        'image_path' => $row['image_path'],
-    ];
-}
-
+// $mysqli = new mysqli('localhost', 'username', 'password', 'database');
+//if ($mysqli->connect_error) {
+//    die('Chyba pripojenia: ' . $mysqli->connect_error);
+//}
+//
+//// Načítanie bannerov z DB
+//$result = $mysqli->query("SELECT * FROM banners ORDER BY id ASC");
+//
+//$bannerSlides = [];
+//while ($row = $result->fetch_assoc()) {
+//    $bannerSlides[] = [
+//        'item_class' => $row['item_class'],
+//        'category' => $row['category'],
+//        'title' => $row['title'],
+//        'image_path' => $row['image_path'],
+//    ];
+//}
+$bannerSlides = [
+    [
+        'item_class' => 'item-1',
+        'category' => 'Our Courses',
+        'title' => 'With Scholar Teachers, Everything Is Easier',
+    ],
+    [
+        'item_class' => 'item-2',
+        'category' => 'Best Result',
+        'title' => 'Get the best result out of your effort',
+    ],
+    [
+        'item_class' => 'item-3',
+        'category' => 'Online Learning',
+        'title' => 'Online Learning helps you save the time',
+    ]
+];
 ?>
 
 <!-- Main Banner Area with taller height -->
